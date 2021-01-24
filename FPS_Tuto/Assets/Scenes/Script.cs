bc497fc782fb3a6a2565f506a3dc8a747550d33e
Mathias Ressort <mathias.ressort@epitech.eu>
2021-01-24 18:27:19 +0100

    [Add] Scene template

---
+++ FPS_Tuto/Assets/Scenes/Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public BoxCollider _box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _box.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
